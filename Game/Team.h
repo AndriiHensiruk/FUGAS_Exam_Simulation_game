1d308694b91e30a530aa15e74eb5ab369a8c6677
AndriiHensiruk <Hensiruk@gmail.com>
2021-11-04 12:06:10 +0200

    worcking  team

--- Game/Team.h
+++ Game/Team.h
@@ -1,1 +1,17 @@
-#pragma once
+#include <string>
+#include <vector>
+class Team
+{
+private:
+    
+    std::string Name;
+    int numTNames;
+public:
+
+    Team();
+    ~Team();
+    std::vector<std::string> nTeamContainer;
+
+
+    
+};
